650c5b63e5bbcfe7556f9bf33d15959f189c83fb
EBEREGIT <samsoneberenjoku@gmail.com>
2019-11-20 23:16:12 +0100

    feature(geocoding): create a function to convert address to cordinates [finish #35]

--- scripts.babel.js
+++ scripts.babel.js
@@ -1,7 +1,39 @@
+// initiate the map here
 function initMap() {
-  let latlng = new google.maps.LatLng(6.459964, 7.548949);
-  let map = new google.maps.Map(document.getElementById("map"), {
-    center: latlng,
-    zoom: 8
+  // create an instance of Map class
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat:  6.459964, lng: 7.548949}
+  });
+
+  // create an instance of Geocoder class 
+  var geocoder = new google.maps.Geocoder();
+
+  // call the geocodeAddress function 
+  document.getElementById('submit').addEventListener('click', function(e) {
+    e.preventDefault();
+    geocodeAddress(geocoder, map);
+  });
+}
+
+// geocoding here
+function geocodeAddress(geocoder, resultsMap) {
+  // get the vlaue of the address input
+  var address = document.getElementById('address').value;
+
+  // convert the address to cordinates here
+  geocoder.geocode({'address': address}, function(results, status) {
+    // check the status. If OK, get the cordinates
+    if (status === 'OK') {
+      // get the cordinates here
+      resultsMap.setCenter(results[0].geometry.location);
+      // place the marker on the location here
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
